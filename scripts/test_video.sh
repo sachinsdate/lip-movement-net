1e8aab3412cfc6129a5605ecaeb94820d278ca13
sachinsdate <sachinsdate@users.noreply.github.com>
2018-06-22 12:36:35 +0530

    Updated test_video.sh

--- scripts/test_video.sh
+++ scripts/test_video.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 clear
-python python ../src/lip_movement_net.py -v ../videos/044598381-carl-rowan-delivering-speech-a.mp4 -p ../models/shape_predictor_68_face_landmarks.dat -m ../models/2_64_True_True_0.25_lip_motion_net_model.h5
+python ../src/lip_movement_net.py -v ../videos/044598381-carl-rowan-delivering-speech-a.mp4 -p ../models/shape_predictor_68_face_landmarks.dat -m ../models/1_32_False_True_0.25_lip_motion_net_model.h5
